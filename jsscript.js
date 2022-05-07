9082f19b41faabdde9fb6db82e446b22db62b777
Dmuyto <Voloshynd13@gmail.com>
2022-05-07 21:00:17 +0200

    Update app

--- jsscript.js
+++ jsscript.js
@@ -1,19 +1,36 @@
-const numberOfFilms = prompt("How many movies did you watch?", "");
+const numberOfFilms = +prompt("How many movies did you watch?", "");
+
+    
 const personalMovieDB  = {
-    count: Number(numberOfFilms),
+    count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
     privat: false
 };
 
-const a = prompt("One of the last movies you watched?", ""),
-    b = Number(prompt("How would you rate it?","")),
-    c =  prompt("One of the last movies you watched?", ""),
-    d = Number(prompt("How would you rate it?",""));
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for( let i = 0; i < 2; i++){
+   
+    const a = prompt("One of the last movies you watched?", "");
+        b = Number(prompt("How would you rate it?",""));
+        if(a !== '' && b !== null && a !== null && b !== "" && a.length < 50){
+        personalMovieDB.movies[a] = b;
+        console.log("Done!");
+        } else{
+            console.log("Error");
+            i--;
+        }
+}
+if (personalMovieDB.count < 10){
+    console.log("You watched just few movies ")
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+    console.log("You are a classic spectator")
+} else if (personalMovieDB.count <=  30){
+    console.log("you are a movie buff")
+} else{
+    console.log("Error")
+}
 
 console.log(personalMovieDB);
         
